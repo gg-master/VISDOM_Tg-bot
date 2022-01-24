1fa748d78004216de6337e04796127ea7eb409c7
Folonder <74664782+Folonder@users.noreply.github.com>
2022-01-24 20:57:57 +0300

    сделал таблицу statistic

--- checking_db.py
+++ checking_db.py
@@ -6,18 +6,18 @@ from data.patient import Patient
 
 def main():
     db_session.global_init()
-    patient = Patient()
-    patient.name = 'Ivanov Ivan Semenovich'
-    patient.user_code = 'IIS112'
-    patient.medicines = 'some_drugs'
-    patient.accept_time_first = datetime.time(hour=5)
-    patient.accept_time_second = datetime.time(hour=17)
-    patient.diff_time = 3
-    patient.chat_id = 'random chat_id1'
-    patient.member = True
-    db_sess = db_session.create_session()
-    db_sess.add(patient)
-    db_sess.commit()
+    # patient = Patient()
+    # patient.name = 'Ivanov Ivan Semenovich'
+    # patient.user_code = 'IIS112'
+    # patient.medicines = 'some_drugs'
+    # patient.accept_time_first = datetime.time(hour=5)
+    # patient.accept_time_second = datetime.time(hour=17)
+    # patient.diff_time = 3
+    # patient.chat_id = 'random chat_id1'
+    # patient.member = True
+    # db_sess = db_session.create_session()
+    # db_sess.add(patient)
+    # db_sess.commit()
 
 
 if __name__ == "__main__":
