8bb4ce7a876af4032ec33f594af4ef94b1b22bcf
gg-master <korshun.tolik@yandex.ru>
2022-02-07 20:50:20 +0300

    Отключил эхо-бот

--- main.py
+++ main.py
@@ -39,13 +39,13 @@ def help_msg(update: Update, context: CallbackContext):
         update.message.reply_text("Справка. Команды для патронажа.")
 
 
-def echo(update: Update, context: CallbackContext):
-    import pytz
-    date = update.message.date
-    tz = pytz.timezone('Etc/Gmt-3')
-    print(tz.normalize(date), '\n', date.astimezone(tz))
-    update.message.reply_text(update.message.text)
-
+# def echo(update: Update, context: CallbackContext):
+#     import pytz
+#     date = update.message.date
+#     tz = pytz.timezone('Etc/Gmt-3')
+#     print(tz.normalize(date), '\n', date.astimezone(tz))
+#     update.message.reply_text(update.message.text)
+#
 
 def main():
     updater = Updater(get_from_env('TOKEN'),
